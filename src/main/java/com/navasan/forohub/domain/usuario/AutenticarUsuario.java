b7a41d27ed47a3107e94479bbf64813cc613f57b
navasan <inaki_nava@hotial.com>
2024-07-16 10:59:29 -0600

    Correccion de errores y autorizacion

---
+++ src/main/java/com/navasan/forohub/domain/usuario/AutenticarUsuario.java
@@ -0,0 +1,2 @@
+package com.navasan.forohub.domain.usuario;public class AutenticarUsuario {
+}
--- src/main/java/com/navasan/forohub/security/AuthenticationManager.java
+++
@@ -1,8 +0,0 @@
-package com.navasan.forohub.security;
-
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.AuthenticationException;
-
-public interface AuthenticationManager {
-    Authentication autenticate(Authentication authentication) throws AuthenticationException;
-}
